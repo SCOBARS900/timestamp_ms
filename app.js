b34ba25dd8ba18ff2fbb6a5cd80e73a2db9ea6d8
Julio Escobar <julioescobar900@gmail.com>
2017-04-21 11:42:32 -0300

    add json response and date parameters

--- app.js
+++ app.js
@@ -3,13 +3,22 @@ var bodyParser = require('body-parser');
 var cors = require('cors');
 
 
-var app = module.exports = express();
+var app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/dateValues', function(req, res, next) {
-  console.log('URL works'); 
+app.get('/date/:dval', function(req, res, next) {
+  var dval = req.params.dval;
+   
+  var dateFormattingOptions = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+  }    
+    
+    
+  res.json({ unix: dval });  
 });
 
 
