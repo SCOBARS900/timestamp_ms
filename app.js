0dfe0fa450566e9ee20bf7c1326745982f534228
Julio Escobar <julioescobar900@gmail.com>
2017-04-25 11:13:38 -0300

    deploying to heroku

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ var app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+app.set('port', (process.env.PORT || 5000));
+
+
+
 app.use(express.static('public'));
 
 
@@ -47,6 +51,6 @@ app.get('/:dval', function(req, res) {
 
 
 
-app.listen(3000, function() {
-   console.log('Local Server ON');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
